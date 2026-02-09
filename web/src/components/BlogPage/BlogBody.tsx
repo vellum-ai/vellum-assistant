451ca9466ad87fde3746419f6e0e5fe02a084c09
ava-reed-assistant <avareedassistant@gmail.com>
2026-02-09 01:27:39 -0500

    remove newsletter and product updates sections from blog page

--- web/src/components/BlogPage/BlogBody.tsx
+++ web/src/components/BlogPage/BlogBody.tsx
@@ -1,7 +1,5 @@
-import { BlogCTA } from "./BlogCTA";
 import { BlogFooter } from "./BlogFooter";
 import { BlogHero } from "./BlogHero";
-import { BlogNewsletter } from "./BlogNewsletter";
 import { NavBar } from "@/components/VellumHomepage/NavBar";
 
 export function BlogBody() {
@@ -9,8 +7,6 @@ export function BlogBody() {
     <div style={{ backgroundColor: "#0d0d0d", minHeight: "100vh" }}>
       <NavBar />
       <BlogHero />
-      <BlogNewsletter />
-      <BlogCTA />
       <BlogFooter />
     </div>
   );
