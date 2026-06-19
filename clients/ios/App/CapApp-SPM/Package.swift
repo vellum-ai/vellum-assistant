3b7629aa1e785defe8e3affcad7d34cc9f7e080d
Noa Flaherty <noa@vellum.ai>
2026-06-18 22:31:03 -0400

    feat(ios): link SentryCapacitor in the committed SPM package

--- clients/ios/App/CapApp-SPM/Package.swift
+++ clients/ios/App/CapApp-SPM/Package.swift
@@ -14,14 +14,15 @@ let package = Package(
         .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", exact: "8.3.4"),
         .package(name: "CapacitorApp", path: "../../../web/node_modules/@capacitor/app"),
         .package(name: "CapacitorBrowser", path: "../../../web/node_modules/@capacitor/browser"),
+        .package(name: "CapacitorFileViewer", path: "../../../web/node_modules/@capacitor/file-viewer"),
         .package(name: "CapacitorFilesystem", path: "../../../web/node_modules/@capacitor/filesystem"),
         .package(name: "CapacitorHaptics", path: "../../../web/node_modules/@capacitor/haptics"),
         .package(name: "CapacitorLocalNotifications", path: "../../../web/node_modules/@capacitor/local-notifications"),
         .package(name: "CapacitorNetwork", path: "../../../web/node_modules/@capacitor/network"),
-        .package(name: "CapacitorPluginSafeArea", path: "../../../web/node_modules/capacitor-plugin-safe-area"),
+        .package(name: "CapacitorPushNotifications", path: "../../../web/node_modules/@capacitor/push-notifications"),
         .package(name: "CapacitorShare", path: "../../../web/node_modules/@capacitor/share"),
-        .package(name: "CapacitorFileViewer", path: "../../../web/node_modules/@capacitor/file-viewer"),
-        .package(name: "CapacitorPushNotifications", path: "../../../web/node_modules/@capacitor/push-notifications")
+        .package(name: "SentryCapacitor", path: "../../../web/node_modules/@sentry/capacitor"),
+        .package(name: "CapacitorPluginSafeArea", path: "../../../web/node_modules/capacitor-plugin-safe-area")
     ],
     targets: [
         .target(
@@ -31,14 +32,15 @@ let package = Package(
                 .product(name: "Cordova", package: "capacitor-swift-pm"),
                 .product(name: "CapacitorApp", package: "CapacitorApp"),
                 .product(name: "CapacitorBrowser", package: "CapacitorBrowser"),
+                .product(name: "CapacitorFileViewer", package: "CapacitorFileViewer"),
                 .product(name: "CapacitorFilesystem", package: "CapacitorFilesystem"),
                 .product(name: "CapacitorHaptics", package: "CapacitorHaptics"),
                 .product(name: "CapacitorLocalNotifications", package: "CapacitorLocalNotifications"),
                 .product(name: "CapacitorNetwork", package: "CapacitorNetwork"),
-                .product(name: "CapacitorPluginSafeArea", package: "CapacitorPluginSafeArea"),
+                .product(name: "CapacitorPushNotifications", package: "CapacitorPushNotifications"),
                 .product(name: "CapacitorShare", package: "CapacitorShare"),
-                .product(name: "CapacitorFileViewer", package: "CapacitorFileViewer"),
-                .product(name: "CapacitorPushNotifications", package: "CapacitorPushNotifications")
+                .product(name: "SentryCapacitor", package: "SentryCapacitor"),
+                .product(name: "CapacitorPluginSafeArea", package: "CapacitorPluginSafeArea")
             ]
         )
     ]
